8479ad3ff9d7e65513ad0b2bac77afd9bcd7c964
Danil Saitov <dvs0707@gmail.com>
2026-03-24 14:57:25 -0400

    Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 // Main App Component
 // Sets up routing for the entire application
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Default route - redirect to login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* Landing page */}
+        <Route path="/" element={<Landing />} />
         
         {/* Authentication routes */}
         <Route path="/login" element={<Login />} />
