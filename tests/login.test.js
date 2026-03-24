1f6de83573ccbcc903c541a450a8d2fa1e962dd6
Isaiah Deppong <isaiahjdeppong@gmail.com>
2026-03-24 16:17:23 -0400

    Extra tests for the login

---
+++ tests/login.test.js
@@ -0,0 +1,89 @@
+const http = require('http');
+const app = require('../src/index');
+const prisma = require('../src/config/database');
+
+let server;
+let baseUrl;
+
+beforeAll(async () => {
+    server = http.createServer(app);
+    await new Promise((done) => {
+        server.listen(0, () => {
+            const { port } = server.address();
+            baseUrl = `http://127.0.0.1:${port}`;
+            done();
+        });
+    });
+});
+
+afterAll(async () => {
+    await new Promise((done) => server.close(done) );
+    await prisma.$disconnect();
+});
+
+describe('signup login apis', ()=> {
+    test('POST /api/auth/signup empty fail', async () => {
+        const res = await(fetch(`${baseUrl}/api/auth/signup`, {
+            method: "post",
+            headers: {invalid: "test"},
+        }));
+        const data = await res.json();
+
+        expect(res.status).toBe(400);
+    });
+
+    test('POST /api/auth/signup not @charlotte.edu', async () => {
+        const res = await(fetch(`${baseUrl}/api/auth/signup`, {
+            method: "post",
+            headers: {
+                username: "example",
+                first_name: "e",
+                last_name: "xample",
+                email: "example@a.com",
+                password: "example123",
+                graduation_year: "2027",
+            }
+        }));
+        const data = await res.json();
+
+        expect(res.status).toBe(400);
+    });
+});
+
+describe('/login login apis', ()=> {
+    test('POST /api/auth/signup empty fail', async () => {
+        const res = await(fetch(`${baseUrl}/api/auth/login`, {
+            method: "post",
+            headers: {
+                empty: "test"
+            },
+        }));
+        const data = await res.json();
+        expect(res.status).toBe(400);
+    });
+
+    test('POST /api/auth/signup invalid email', async () => {
+        const res = await(fetch(`${baseUrl}/api/auth/login`, {
+            method: "post",
+            headers: {
+                email: "!(*@&^(*^#(@*&",
+                password: "example123",
+            },
+        }));
+        const data = await res.json();
+        expect(res.status).toBe(401);
+    });
+
+    test('POST /api/auth/signup invalid pass', async () => {
+        const res = await(fetch(`${baseUrl}/api/auth/login`, {
+            method: "post",
+            headers: {
+                email: "example@charlotte.edu",
+                password: "wrong",
+            },
+        }));
+        const data = await res.json();
+        expect(res.status).toBe(401);
+    });
+});
+
